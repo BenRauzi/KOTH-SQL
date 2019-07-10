45c138391d453fcf3d630948f70c7cab800fdf1f
Ben Rauzi <ben.rauzi@outlook.com>
2019-07-11 00:34:26 +1200

    Update KOTH.sql

--- Harris/KOTH.sql
+++ Harris/KOTH.sql
@@ -7,6 +7,5 @@ USE `KOTH`;
 CREATE TABLE IF NOT EXISTS `users` (
   `uid` varchar(50) DEFAULT NULL,
   `data` varchar(500) DEFAULT NULL,
-  PRIMARY KEY (`user`),
-  UNIQUE KEY `uid` (`uid`)
+  PRIMARY KEY `uid` (`uid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
